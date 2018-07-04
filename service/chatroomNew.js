8cdbfb224e4476a61c01c378011fb35015da848e
shutup <fyjc999@gmail.com>
2018-07-04 17:55:39 +0800

    use normal code style

--- service/chatroomNew.js
+++ service/chatroomNew.js
@@ -3,16 +3,16 @@ var server = require('../bin/www');
 var io = require('socket.io')(server);
 
 
-io.on('connection', (socket) => {
+io.on('connection', function (socket){
 
-    socket.on('sign in',(data)=>{
+    socket.on('sign in',function (data){
         socket.username = data;
         socket.broadcast.emit('user in',data);
         socket.emit('sign in ok',data);
     });
 
     // when the client emits 'new message', this listens and executes
-    socket.on('new message', (data) => {
+    socket.on('new message', function (data) {
         // we tell the client to execute 'new message'
         socket.broadcast.emit('new message', {
             username: socket.username,
